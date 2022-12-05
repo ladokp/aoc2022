0447414119627b552f2516da53bbecabc86e3370
Patrick Kodal <patrick.kodal@dat.at>
2022-12-05 15:16:04 +0100

    improved 2022/05 solution

--- solution/aoc_day_05.py
+++ solution/aoc_day_05.py
@@ -10,10 +10,10 @@ class AocSolution(AocBaseClass):
     def _parse(self, puzzle_input):
         """Parse input"""
         starting_stacks, instructions = puzzle_input.split("\n\n")
-        starting_stacks_list = starting_stacks.split("\n")
-        starting_stacks_list.pop(-1)  # delete stack labels
+        starting_stacks = starting_stacks.split("\n")
+        starting_stacks.pop(-1)  # delete stack labels
         stacks = defaultdict(deque)
-        for line in starting_stacks_list:
+        for line in starting_stacks:
             for position in range(1, len(line), 4):
                 i = 1 + (position - 1) // 4
                 if line[position].strip():
@@ -26,21 +26,22 @@ class AocSolution(AocBaseClass):
 
     DAY = 5
 
-    def part1(self):
-        """Solve part 1"""
+    def _move_crates(self, /, crate_mover_version=9000):
         stacks = deepcopy(self.data[0])
         for amount, origin, destination in self.data[1]:
-            for _ in range(amount):
-                stacks[destination].append(stacks[origin].pop())
+            crates_to_move = [stacks[origin].pop() for _ in range(amount)]
+            if crate_mover_version == 9001:
+                crates_to_move = reversed(crates_to_move)
+            stacks[destination].extend(crates_to_move)
         return "".join([stacks[i][-1] for i in sorted(stacks)])
+
+    def part1(self):
+        """Solve part 1"""
+        return self._move_crates()
 
     def part2(self):
         """Solve part 2"""
-        stacks = deepcopy(self.data[0])
-        for amount, origin, destination in self.data[1]:
-            move_stack = reversed([stacks[origin].pop() for _ in range(amount)])
-            stacks[destination].extend(move_stack)
-        return "".join([stacks[i][-1] for i in sorted(stacks)])
+        return self._move_crates(crate_mover_version=9001)
 
 
 if __name__ == "__main__":
