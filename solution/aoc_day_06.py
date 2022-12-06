37374f085e6c326015375a78eef6b72549b32dcd
Patrick Kodal <patrick.kodal@dat.at>
2022-12-06 10:16:57 +0100

    improved 2022/06 solution

--- solution/aoc_day_06.py
+++ solution/aoc_day_06.py
@@ -1,7 +1,6 @@
 # aoc_day_06.py
 
 from solution.aoc_base import AocBaseClass
-from collections import Counter
 
 
 class AocSolution(AocBaseClass):
@@ -11,19 +10,22 @@ class AocSolution(AocBaseClass):
 
     DAY = 6
 
-    def _get_end_index_of_first_distinct_frame(self, frame_length):
-        for index in range(0, len(self.data)):
-            word_count = len(Counter(self.data[index : index + frame_length]).values())
-            if word_count == frame_length:
-                return index + frame_length
+    def _inspect_first_distinct_frame(self, frame_length):
+        current_frame = []
+        for index, character in enumerate(self.data, start=1):
+            if character in current_frame:
+                current_frame = current_frame[current_frame.index(character) + 1 :]
+            current_frame.append(character)
+            if len(current_frame) == frame_length:
+                return index
 
     def part1(self):
         """Solve part 1"""
-        return self._get_end_index_of_first_distinct_frame(4)
+        return self._inspect_first_distinct_frame(4)
 
     def part2(self):
         """Solve part 2"""
-        return self._get_end_index_of_first_distinct_frame(14)
+        return self._inspect_first_distinct_frame(14)
 
 
 if __name__ == "__main__":
