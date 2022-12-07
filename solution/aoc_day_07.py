7df75dcaecaf55a4c00e422af5059ba73f4246f0
Patrick Kodal <patrick@kodal.dev>
2022-12-07 21:09:41 +0100

    improved 2022/07 solution

--- solution/aoc_day_07.py
+++ solution/aoc_day_07.py
@@ -9,7 +9,7 @@ class AocSolution(AocBaseClass):
     def _parse(self, puzzle_input):
         """Parse input"""
         return self._build_directory_tree(
-            [line.strip() for line in puzzle_input.split("\n")]
+            tuple(line.strip() for line in puzzle_input.split("\n"))
         )
 
     DAY = 7
@@ -17,32 +17,27 @@ class AocSolution(AocBaseClass):
     @staticmethod
     def _build_directory_tree(history):
         total_sizes, current_position, folder_stack = defaultdict(lambda: 0), 1, [""]
-        while True:
+        while current_position < len(history):
             current_row = history[current_position]
-            if current_row.startswith("$"):
-                match current_row.split()[1:]:
-                    case ["cd", ".."]:
-                        folder_stack.pop()
-                        current_position += 1
-                    case ["cd", folder]:
-                        folder_stack.append(folder)
-                        current_position += 1
-                    case ["ls"]:
-                        new_position = current_position + 1
-                        while new_position < len(history):
-                            command = history[new_position]
-                            if command.startswith("$"):
-                                break
-                            elif not command.startswith("dir"):
-                                size = int(command.split()[0])
-                                for index in range(len(folder_stack)):
-                                    total_sizes[
-                                        "/".join(folder_stack[: index + 1])
-                                    ] += size
-                            new_position += 1
-                        current_position = new_position
-                if current_position >= len(history):
-                    break
+            match current_row.split():
+                case ["$", "cd", ".."]:
+                    folder_stack.pop()
+                    current_position += 1
+                case ["$", "cd", folder]:
+                    folder_stack.append(folder)
+                    current_position += 1
+                case ["$", "ls"]:
+                    new_position = current_position + 1
+                    while new_position < len(history):
+                        command = history[new_position]
+                        if command.startswith("$"):
+                            break
+                        elif not command.startswith("dir"):
+                            size = int(command.split()[0])
+                            for index in range(len(folder_stack)):
+                                total_sizes["/".join(folder_stack[: index + 1])] += size
+                        new_position += 1
+                    current_position = new_position
         return total_sizes
 
     def part1(self):
