ef012a3f68ba67eba0f447d299635d05e677aef1
Patrick Kodal <patrick@kodal.dev>
2022-12-08 10:45:18 +0100

    improved 2022/08 solution

--- solution/aoc_day_08.py
+++ solution/aoc_day_08.py
@@ -7,127 +7,134 @@ from solution.aoc_base import AocBaseClass
 class AocSolution(AocBaseClass):
     def _parse(self, puzzle_input):
         """Parse input"""
-        puzzle_input = puzzle_input.split("\n")
-        tree_map = [[int(height) for height in line] for line in puzzle_input]
+        tree_map = tuple(
+            tuple(int(height) for height in line) for line in puzzle_input.split("\n")
+        )
         tree_visibility_mask = []
         for line_index, line in enumerate(tree_map):
             tree_visibility_mask.append([])
             for tree_index, tree in enumerate(line):
-                if (
-                    line_index == 0
-                    or line_index == len(tree_map) - 1
-                    or tree_index == 0
-                    or tree_index == len(line) - 1
+                if line_index in (0, len(tree_map) - 1) or tree_index in (
+                    0,
+                    len(line) - 1,
                 ):
                     tree_visibility_mask[line_index].append(1)
                 else:
                     tree_visibility_mask[line_index].append(0)
-        return tree_map, tree_visibility_mask
+        return self._score_trees(tree_map, tree_visibility_mask)
 
     DAY = 8
 
-    def _is_left_smaller(self, line_index, column_index):
+    @staticmethod
+    def _is_left_smaller(tree_map, line_index, column_index):
         return (
-            max(self.data[0][line_index][0:column_index])
-            < self.data[0][line_index][column_index]
+            max(tree_map[line_index][0:column_index])
+            < tree_map[line_index][column_index]
         )
 
-    def _is_right_smaller(self, line_index, column_index):
+    @staticmethod
+    def _is_right_smaller(tree_map, line_index, column_index):
         return (
-            max(self.data[0][line_index][column_index + 1 : len(self.data[0])])
-            < self.data[0][line_index][column_index]
+            max(tree_map[line_index][column_index + 1 : len(tree_map)])
+            < tree_map[line_index][column_index]
         )
 
-    def _is_upper_smaller(self, line_index, column_index):
-        upper_heights = [
-            self.data[0][index][column_index] for index in range(line_index)
-        ]
-        return max(upper_heights) < self.data[0][line_index][column_index]
+    @staticmethod
+    def _is_upper_smaller(tree_map, line_index, column_index):
+        upper_heights = [tree_map[index][column_index] for index in range(line_index)]
+        return max(upper_heights) < tree_map[line_index][column_index]
 
-    def _is_lower_smaller(self, line_index, column_index):
+    @staticmethod
+    def _is_lower_smaller(tree_map, line_index, column_index):
         lower_heights = [
-            self.data[0][index][column_index]
-            for index in range(line_index + 1, len(self.data[0]))
+            tree_map[index][column_index]
+            for index in range(line_index + 1, len(tree_map))
         ]
-        return max(lower_heights) < self.data[0][line_index][column_index]
+        return max(lower_heights) < tree_map[line_index][column_index]
 
-    def _get_left_score(self, line_index, column_index):
-        current_height = self.data[0][line_index][column_index]
-        score_counter = 0
-        if column_index == 0:
+    @staticmethod
+    def _get_left_score(tree_map, line_index, column_index):
+        current_height = tree_map[line_index][column_index]
+        score = 0
+        if not column_index:
             return 0
-        for height in reversed(self.data[0][line_index][:column_index]):
-            score_counter += 1
+        for height in reversed(tree_map[line_index][:column_index]):
+            score += 1
             if height >= current_height:
                 break
-        return score_counter
+        return score
 
-    def _get_right_score(self, line_index, column_index):
-        current_height = self.data[0][line_index][column_index]
-        score_counter = 0
-        if column_index == len(self.data[0][line_index]):
+    @staticmethod
+    def _get_right_score(tree_map, line_index, column_index):
+        current_height = tree_map[line_index][column_index]
+        score = 0
+        if column_index == len(tree_map[line_index]):
             return 0
-        for height in self.data[0][line_index][column_index + 1 :]:
-            score_counter += 1
+        for height in tree_map[line_index][column_index + 1 :]:
+            score += 1
             if height >= current_height:
                 break
-        return score_counter
+        return score
 
-    def _get_lower_score(self, line_index, column_index):
-        current_height = self.data[0][line_index][column_index]
-        score_counter = 0
-        if line_index == len(self.data[0]) - 1:
+    @staticmethod
+    def _get_lower_score(tree_map, line_index, column_index):
+        current_height = tree_map[line_index][column_index]
+        score = 0
+        if line_index == len(tree_map) - 1:
             return 0
-        for height in self.data[0][line_index + 1 :]:
+        for height in tree_map[line_index + 1 :]:
             height = height[column_index]
-            score_counter += 1
+            score += 1
             if height >= current_height:
                 break
-        return score_counter
+        return score
 
-    def _get_upper_score(self, line_index, column_index):
-        current_height = self.data[0][line_index][column_index]
-        score_counter = 0
-        if line_index == 0:
+    @staticmethod
+    def _get_upper_score(tree_map, line_index, column_index):
+        current_height = tree_map[line_index][column_index]
+        score = 0
+        if not line_index:
             return 0
-        for height in reversed(self.data[0][:line_index]):
+        for height in reversed(tree_map[:line_index]):
             height = height[column_index]
-            score_counter += 1
+            score += 1
             if height >= current_height:
                 break
-        return score_counter
+        return score
 
-    def part1(self):
-        """Solve part 1"""
-        seen_tree_counter = 0
-        tree_map, tree_visibility_mask = self.data[0], deepcopy(self.data[1])
+    def _score_trees(self, tree_map, scoring_mask):
+        tree_visibility_mask, tree_scores = deepcopy(scoring_mask), deepcopy(
+            scoring_mask
+        )
+        seen_trees_counter = 0
         for line_index, line in enumerate(tree_map):
             for column_index, tree in enumerate(line):
                 if tree_visibility_mask[line_index][column_index] == 1:
-                    seen_tree_counter += 1
+                    seen_trees_counter += 1
                 elif (
-                    self._is_left_smaller(line_index, column_index)
-                    or self._is_right_smaller(line_index, column_index)
-                    or self._is_upper_smaller(line_index, column_index)
-                    or self._is_lower_smaller(line_index, column_index)
+                    self._is_left_smaller(tree_map, line_index, column_index)
+                    or self._is_right_smaller(tree_map, line_index, column_index)
+                    or self._is_upper_smaller(tree_map, line_index, column_index)
+                    or self._is_lower_smaller(tree_map, line_index, column_index)
                 ):
                     tree_visibility_mask[line_index][column_index] = 1
-                    seen_tree_counter += 1
-        return seen_tree_counter
+                    seen_trees_counter += 1
+                tree_scores[line_index][column_index] = (
+                    self._get_upper_score(tree_map, line_index, column_index)
+                    * self._get_left_score(tree_map, line_index, column_index)
+                    * self._get_lower_score(tree_map, line_index, column_index)
+                    * self._get_right_score(tree_map, line_index, column_index)
+                )
+            tree_scores[line_index] = max(tree_scores[line_index])
+        return seen_trees_counter, max(tree_scores)
+
+    def part1(self):
+        """Solve part 1"""
+        return self.data[0]
 
     def part2(self):
         """Solve part 2"""
-        tree_map, tree_visibility_mask = self.data[0], deepcopy(self.data[1])
-        for line_index, line in enumerate(tree_map):
-            for column_index, tree in enumerate(line):
-                tree_visibility_mask[line_index][column_index] = (
-                    self._get_upper_score(line_index, column_index)
-                    * self._get_left_score(line_index, column_index)
-                    * self._get_lower_score(line_index, column_index)
-                    * self._get_right_score(line_index, column_index)
-                )
-            tree_visibility_mask[line_index] = max(tree_visibility_mask[line_index])
-        return max(tree_visibility_mask)
+        return self.data[1]
 
 
 if __name__ == "__main__":
