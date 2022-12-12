f877461c75e690dfa40f6842c6581591eb2e7b05
Patrick Kodal <patrick.kodal@dat.at>
2022-12-12 08:04:24 +0100

    improved 2022/12 solution

--- solution/aoc_day_12.py
+++ solution/aoc_day_12.py
@@ -5,67 +5,94 @@ from solution.aoc_base import AocBaseClass
 
 
 class AocSolution(AocBaseClass):
-    def __init__(self, /, test_suffix=""):
-        super().__init__(test_suffix)
-        self.graph = None
-
     def _parse(self, puzzle_input):
         """Parse input"""
-        sx, sy = -1, -1
-        ex, ey = -1, -1
+        s_x, s_y = -1, -1
+        e_x, e_y = -1, -1
         board = []
-        for row, s in enumerate(puzzle_input.split("\n")):
+        for row_index, row in enumerate(puzzle_input.split("\n")):
             vals = []
-            for col, c in enumerate(s):
-                match c:
+            for col_index, col in enumerate(row):
+                match col:
                     case "S":
                         height = 0
-                        sx, sy = row, col
+                        s_x, s_y = row_index, col_index
                     case "E":
                         height = 25
-                        ex, ey = row, col
+                        e_x, e_y = row_index, col_index
                     case _:
-                        height = ord(c) - ord("a")
+                        height = ord(col) - ord("a")
                 vals.append(height)
             board.append(vals)
-        return board, (sx, sy), (ex, ey)
+        return board, (s_x, s_y), (e_x, e_y)
 
     DAY = 12
 
-    def part1(self):
-        """Solve part 1"""
-        self.graph = graph = dijkstar.Graph()
+    def _find_shortest_path(self):
+        graph = dijkstar.Graph()
         board, start, end = self.data
-        for r, row in enumerate(board):
-            for c, height in enumerate(row):
-                if c > 0 and board[r][c - 1] <= height + 1:
-                    graph.add_edge((r, c), (r, c - 1), 1)
-                if c < len(row) - 1 and board[r][c + 1] <= height + 1:
-                    graph.add_edge((r, c), (r, c + 1), 1)
-                if r > 0 and board[r - 1][c] <= height + 1:
-                    graph.add_edge((r, c), (r - 1, c), 1)
-                if r < len(board) - 1 and board[r + 1][c] <= height + 1:
-                    graph.add_edge((r, c), (r + 1, c), 1)
-        return dijkstar.find_path(graph, start, end).total_cost
-
-    def part2(self):
-        """Solve part 2"""
+        for row_index, row in enumerate(board):
+            for column_index, height in enumerate(row):
+                if column_index > 0 and row[column_index - 1] <= height + 1:
+                    graph.add_edge(
+                        (row_index, column_index),
+                        (row_index, column_index - 1),
+                        1,
+                    )
+                if (
+                    column_index < len(row) - 1
+                    and row[column_index + 1] <= height + 1
+                ):
+                    graph.add_edge(
+                        (row_index, column_index),
+                        (row_index, column_index + 1),
+                        1,
+                    )
+                if (
+                    row_index > 0
+                    and board[row_index - 1][column_index] <= height + 1
+                ):
+                    graph.add_edge(
+                        (row_index, column_index),
+                        (row_index - 1, column_index),
+                        1,
+                    )
+                if (
+                    row_index < len(board) - 1
+                    and board[row_index + 1][column_index] <= height + 1
+                ):
+                    graph.add_edge(
+                        (row_index, column_index),
+                        (row_index + 1, column_index),
+                        1,
+                    )
+        part1 = dijkstar.find_path(graph, start, end).total_cost
         best = 1_000_000
-        if not self.graph:
-            self.part1()
-        board, start, end = self.data
-        for r, row in enumerate(board):
-            for c, height in enumerate(row):
+        for row_index, row in enumerate(board):
+            for column_index, height in enumerate(row):
                 if height == 0:
                     try:
                         cost = dijkstar.find_path(
-                            self.graph, (r, c), end
+                            graph, (row_index, column_index), end
                         ).total_cost
                     except dijkstar.algorithm.NoPathError:
                         continue
                     if cost < best:
                         best = cost
-        return best
+        self.solved = True
+        self.solutions = part1, best
+
+    def part1(self):
+        """Solve part 1"""
+        if not self.solved:
+            self._find_shortest_path()
+        return self.solutions[0]
+
+    def part2(self):
+        """Solve part 2"""
+        if not self.solved:
+            self._find_shortest_path()
+        return self.solutions[1]
 
 
 if __name__ == "__main__":
