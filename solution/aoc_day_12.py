782e36379bcaf1400e2a2b17084f6c29c871c272
Patrick Kodal <patrick.kodal@dat.at>
2022-12-12 13:27:40 +0100

    improved 2022/12 solution

--- solution/aoc_day_12.py
+++ solution/aoc_day_12.py
@@ -1,5 +1,4 @@
 # aoc_day_12.py
-import dijkstar
 
 from solution.aoc_base import AocBaseClass
 
@@ -7,92 +6,46 @@ from solution.aoc_base import AocBaseClass
 class AocSolution(AocBaseClass):
     def _parse(self, puzzle_input):
         """Parse input"""
-        s_x, s_y = -1, -1
-        e_x, e_y = -1, -1
-        board = []
-        for row_index, row in enumerate(puzzle_input.split("\n")):
-            vals = []
-            for col_index, col in enumerate(row):
-                match col:
-                    case "S":
-                        height = 0
-                        s_x, s_y = row_index, col_index
-                    case "E":
-                        height = 25
-                        e_x, e_y = row_index, col_index
-                    case _:
-                        height = ord(col) - ord("a")
-                vals.append(height)
-            board.append(vals)
-        return board, (s_x, s_y), (e_x, e_y)
+        grid, start, end = {}, None, None
+        for line_index, line in enumerate(puzzle_input.splitlines()):
+            for column_index, column in enumerate(line):
+                if column == "S":
+                    start = (column_index, line_index)
+                    grid[start] = 0
+                elif column == "E":
+                    end = (column_index, line_index)
+                    grid[end] = 25
+                else:
+                    grid[column_index, line_index] = ord(column) - ord("a")
+        return grid, start, end
 
     DAY = 12
 
-    def _find_shortest_path(self):
-        graph = dijkstar.Graph()
-        board, start, end = self.data
-        for row_index, row in enumerate(board):
-            for column_index, height in enumerate(row):
-                if column_index > 0 and row[column_index - 1] <= height + 1:
-                    graph.add_edge(
-                        (row_index, column_index),
-                        (row_index, column_index - 1),
-                        1,
-                    )
+    def solve_breadth_first(self, *, part2=False):
+        grid, start, end = self.data
+        visited = set()
+        path = [(0, end)]
+        while path:
+            steps, p_0 = path.pop(0)
+            if grid[p_0] == 0 and (part2 or p_0 == start):
+                return steps
+            for d_x, d_y in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                p_1 = p_0[0] + d_x, p_0[1] + d_y
                 if (
-                    column_index < len(row) - 1
-                    and row[column_index + 1] <= height + 1
+                    p_1 not in visited
+                    and p_1 in grid
+                    and grid[p_1] >= grid[p_0] - 1
                 ):
-                    graph.add_edge(
-                        (row_index, column_index),
-                        (row_index, column_index + 1),
-                        1,
-                    )
-                if (
-                    row_index > 0
-                    and board[row_index - 1][column_index] <= height + 1
-                ):
-                    graph.add_edge(
-                        (row_index, column_index),
-                        (row_index - 1, column_index),
-                        1,
-                    )
-                if (
-                    row_index < len(board) - 1
-                    and board[row_index + 1][column_index] <= height + 1
-                ):
-                    graph.add_edge(
-                        (row_index, column_index),
-                        (row_index + 1, column_index),
-                        1,
-                    )
-        part1 = dijkstar.find_path(graph, start, end).total_cost
-        best = 1_000_000
-        for row_index, row in enumerate(board):
-            for column_index, height in enumerate(row):
-                if height == 0:
-                    try:
-                        cost = dijkstar.find_path(
-                            graph, (row_index, column_index), end
-                        ).total_cost
-                    except dijkstar.algorithm.NoPathError:
-                        continue
-                    if cost < best:
-                        best = cost
-        self.solved = True
-        self.solutions = part1, best
+                    visited.add(p_1)
+                    path.append((steps + 1, p_1))
 
     def part1(self):
         """Solve part 1"""
-        if not self.solved:
-            self._find_shortest_path()
-        return self.solutions[0]
+        return self.solve_breadth_first()
 
     def part2(self):
         """Solve part 2"""
-        if not self.solved:
-            self._find_shortest_path()
-        return self.solutions[1]
+        return self.solve_breadth_first(part2=True)
 
 
 if __name__ == "__main__":
